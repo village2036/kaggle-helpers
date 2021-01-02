3cb9f7c6f608b61282a5e434e94017de41c5ce90
Jérémy Viau-Trudel <jeremy.vtrudel@gmail.com>
2021-01-02 04:57:12 -0500

    Use only package village2036

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(name='kaggle-utilities',
       author='village2036',
       author_email='dev@village2036.com',
       url='https://github.com/village2036/kaggle-utilities',
-      packages=['kaggle_utils', 'village2036'],
+      packages=['village2036'],
       requires=['numpy','pandas', 'geopandas','matplotlib','kaggle']
      )
